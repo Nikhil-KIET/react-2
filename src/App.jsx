96e513355ad303320518bd51f269df1f9379e1a4
Nikhil-KIET <95426399+Nikhil-KIET@users.noreply.github.com>
2024-05-01 22:44:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Topics Covered</h1>
+      <br />
+      <p>The following is a list of all the topics we cover in the MDN learning area.</p>
+      <br />
+      <a href="" className="">Getting started with the web</a>
+      <br />
+      <p>Provides a practical introduction to web development for complete beginners.</p>
+      <br />
+      <a href="">HTML — Structuring the web</a>
+      <br />
+      <p>HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.</p>
+      <br />
+      <a href="">CSS — Styling the web</a>
+      <br />
+      <p>CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.</p>
+    </div>
+  );
+}
+
+export default App;
